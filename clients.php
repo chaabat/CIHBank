bdfb657ce3b315466e3155e3cb167c01f25bd6c2
Ayoub CHAABAT <chaabat.02@gmail.com>
2023-11-28 17:10:47 +0100

    vibank

--- clients.php
+++ clients.php
@@ -96,7 +96,9 @@ include('sidenavclient.php');
                 ' . $row['familyName'] . '
                 </td>
                 <td class="px-6 py-4">
-                    <a href="comptsclient.php" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Show</a>
+                
+                <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><a href="comptsclient.php">Aficher</a></button>
+
                 </td>
                 
             </tr>';
--- comptsclient.php
+++ comptsclient.php
@@ -91,7 +91,9 @@ include('sidenavclient.php');
                 </td>
                
                 <td class="px-6 py-4">
-                    <a href="transactionsclient.php" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Show</a>
+                <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"> <a href="transactionsclient.php">Afficher</a></button>
+
+                   
                 </td>
                 
             </tr>';
